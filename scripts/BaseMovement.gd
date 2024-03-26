98897418b1dc6f928a785d8ddd61d9631d5a7fc6
Kuduro <lapizgaming7751@gmail.com>
2024-03-26 16:34:39 +0800

    State Machine test + 4.2 upgrade
    Upgraded Godot to 4.2. Building state machine for character movement and combat

---
+++ scripts/BaseMovement.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+# Stats
+@export var baseSpeed : float = 10.0
+@export var maxHealth : float = 1500
+
+# ModSpeed
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ scripts/BattleMovement.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+# Vars
+@export var baseSpeed : float = 10.0
+@export var maxHP : float = 1500
+@export var elm : Array = ["empty","joy","sorrow","vengance","fear","power","wealth","fame"]
+
+# Statss
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
